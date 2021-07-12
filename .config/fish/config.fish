2f1319459c421ab94e20bc690a6587a115bc6f02
amit-lab <amitdongarwar1810@gmail.com>
2021-07-12 19:19:23 +0530

    added awesome and fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+# My alias
+alias p="python"
+alias pycharm="cd /opt/pycharm/bin/ && ./pycharm.sh"
+alias ls="exa"
+alias cat="bat"
+alias music="ncmpcpp"
+alias note="sncli"
+alias v="nvim"
+
+# My environment variables
+set -gx EDITOR vim
+set -gx SNCLIRC $HOME/.config/snclirc/rc 
+
+
+function fish_greeting
+end
+
+neofetch
